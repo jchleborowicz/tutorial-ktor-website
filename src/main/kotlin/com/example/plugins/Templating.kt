0a77bf640db8ff959b4937ae0b9f3050aaca8406
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2023-08-30 17:01:57 +0200

    Initial commit.

--- src/main/kotlin/com/example/plugins/Templating.kt
+++ src/main/kotlin/com/example/plugins/Templating.kt
@@ -1,14 +1,11 @@
 package com.example.plugins
 
-import freemarker.cache.*
+import freemarker.cache.ClassTemplateLoader
 import io.ktor.server.application.*
 import io.ktor.server.freemarker.*
-import io.ktor.server.response.*
 
 fun Application.configureTemplating() {
     install(FreeMarker) {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
-    }
-    routing {
     }
 }
